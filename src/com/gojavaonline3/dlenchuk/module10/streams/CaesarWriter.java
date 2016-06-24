b1995a3c3981dadfc8aa23e7396059c763ba3773
Dmitrij0 <dmitrijlenchuk@gmail.com>
2016-06-24 09:44:38 +0300

    Javadocs

--- src/com/gojavaonline3/dlenchuk/module10/streams/CaesarWriter.java
+++ src/com/gojavaonline3/dlenchuk/module10/streams/CaesarWriter.java
@@ -6,24 +6,49 @@ import com.gojavaonline3.dlenchuk.module09.cipher.CaesarEncoder;
 import java.io.IOException;
 import java.io.Writer;
 
+/**
+ * Decodes encoded a char stream
+ *
+ * @see java.io.Writer
+ * @see Caesar
+ * @see CaesarEncoder
+ * @see CaesarReader
+ *
+ * @author Dmitrij Lenchuk
+ * @since 24.06.2016
+ */
 class CaesarWriter extends Writer {
 
+    /**The cipher object. The object implements the Caesar encryption */
     private Caesar caesar;
+
+    /**Output stream */
     private Writer out;
 
-
+    /**
+     * Creates a encoding character-output stream that uses an Caesar algorithm of
+     * the specified shift.
+     *
+     * @param  out      A Writer
+     * @param  shift    A Caesar Algorithm Shift
+     */
     CaesarWriter(Writer out, int shift) {
         this.out = out;
         this.caesar = new Caesar(shift);
     }
 
-    public CaesarWriter(Object lock, Writer out, int shift) {
-        super(lock);
-        this.out = out;
-        this.caesar = new Caesar(shift);
-    }
-
-    @Override
+    /**
+     * Writes a portion of an encrypted array of characters.
+     *
+     * <p> Ordinarily this method stores characters from the given array into
+     * this stream's buffer.
+     *
+     * @param  cbuf  A character array
+     * @param  off   Offset from which to start reading characters
+     * @param  len   Number of characters to write
+     *
+     * @exception  IOException  If an I/O error occurs
+     */
     public void write(char[] cbuf, int off, int len) throws IOException {
         synchronized (lock) {
             ensureOpen();
@@ -32,16 +57,17 @@ class CaesarWriter extends Writer {
         }
     }
 
+    /** Checks to make sure that the stream has not been closed */
     private void ensureOpen() throws IOException {
         if (out == null)
             throw new IOException("Stream closed");
     }
 
-    @Override
+    /**The class hasn't a buffer*/
     public void flush() throws IOException {
     }
 
-    @Override
+    /**Closes a stream*/
     public void close() throws IOException {
         synchronized (lock) {
             if (out == null)
