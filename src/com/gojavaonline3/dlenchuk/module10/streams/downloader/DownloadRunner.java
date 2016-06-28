aa108e919fa8dab01acc838228e98bc665ec115e
Dmitrij0 <dmitrijlenchuk@gmail.com>
2016-06-28 20:52:23 +0300

    Easy Fix

--- src/com/gojavaonline3/dlenchuk/module10/streams/downloader/DownloadRunner.java
+++ src/com/gojavaonline3/dlenchuk/module10/streams/downloader/DownloadRunner.java
@@ -35,20 +35,4 @@ public class DownloadRunner {
         }
     }
 
-//    public static void downloadFile() throws MalformedURLException  {
-//        URL url = new URL("http://www.ex.ua/102304751?r=70538,23775");
-//        try (InputStream in = new BufferedInputStream(url.openStream());
-//             OutputStream outConsole = new BufferedOutputStream(System.out);
-//             OutputStream outFile = new BufferedOutputStream(new FileOutputStream(new File("c:/Temp/ex_ua.html")))) {
-//            int byteCount;
-//            byte[] byteBuffer = new byte[8192];
-//            while ((byteCount = in.read(byteBuffer)) != -1) {
-//                outConsole.write(byteBuffer, 0, byteCount);
-//                outFile.write(byteBuffer, 0, byteCount);
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
 }
