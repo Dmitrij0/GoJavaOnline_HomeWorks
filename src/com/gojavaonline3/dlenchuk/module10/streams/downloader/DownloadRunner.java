e67664f3d2cef1edbf9c23e3d080d8e5a75e1e4c
Dmitrij0 <dmitrijlenchuk@gmail.com>
2016-06-28 20:50:12 +0300

    Easy Fix

--- src/com/gojavaonline3/dlenchuk/module10/streams/downloader/DownloadRunner.java
+++ src/com/gojavaonline3/dlenchuk/module10/streams/downloader/DownloadRunner.java
@@ -1,7 +1,6 @@
 package com.gojavaonline3.dlenchuk.module10.streams.downloader;
 
 import java.io.*;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 import java.util.Set;
@@ -12,7 +11,7 @@ public class DownloadRunner {
 
     public static void main(String[] args) throws IOException {
 
-        FileListener fileListener = new FileListener("download.log");
+        FileListener fileListener = new FileListener(System.getProperty("java.io.tmpdir") + "download.log");
         ConsoleListener consoleListener = new ConsoleListener();
         try {
             System.out.println("Please, give a link:");
@@ -36,20 +35,20 @@ public class DownloadRunner {
         }
     }
 
-    public static void downloadFile() throws MalformedURLException  {
-        URL url = new URL("http://www.ex.ua/102304751?r=70538,23775");
-        try (InputStream in = new BufferedInputStream(url.openStream());
-             OutputStream outConsole = new BufferedOutputStream(System.out);
-             OutputStream outFile = new BufferedOutputStream(new FileOutputStream(new File("c:/Temp/ex_ua.html")))) {
-            int byteCount;
-            byte[] byteBuffer = new byte[8192];
-            while ((byteCount = in.read(byteBuffer)) != -1) {
-                outConsole.write(byteBuffer, 0, byteCount);
-                outFile.write(byteBuffer, 0, byteCount);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
+//    public static void downloadFile() throws MalformedURLException  {
+//        URL url = new URL("http://www.ex.ua/102304751?r=70538,23775");
+//        try (InputStream in = new BufferedInputStream(url.openStream());
+//             OutputStream outConsole = new BufferedOutputStream(System.out);
+//             OutputStream outFile = new BufferedOutputStream(new FileOutputStream(new File("c:/Temp/ex_ua.html")))) {
+//            int byteCount;
+//            byte[] byteBuffer = new byte[8192];
+//            while ((byteCount = in.read(byteBuffer)) != -1) {
+//                outConsole.write(byteBuffer, 0, byteCount);
+//                outFile.write(byteBuffer, 0, byteCount);
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
 }
