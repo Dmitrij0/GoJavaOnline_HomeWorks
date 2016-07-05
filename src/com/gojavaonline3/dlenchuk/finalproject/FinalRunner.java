59c13153b200801eaf7235e4e9e2fd93842b81a8
Dmitrij0 <dmitrijlenchuk@gmail.com>
2016-07-05 04:39:21 +0300

    Final project. Factorial and fibonacci are commited.

---
+++ src/com/gojavaonline3/dlenchuk/finalproject/FinalRunner.java
@@ -0,0 +1,26 @@
+package com.gojavaonline3.dlenchuk.finalproject;
+
+import com.gojavaonline3.dlenchuk.finalproject.factorial.Factorial;
+import com.gojavaonline3.dlenchuk.finalproject.factorial.Fibonacci;
+
+import java.math.BigInteger;
+
+public class FinalRunner {
+
+    public static void main(String[] args) {
+        final Factorial factorial = new Factorial(100);
+
+        long timePoint = System.nanoTime();
+        System.out.println(factorial.calculate());
+        System.out.println("Elapsed Time: " + (System.nanoTime() - timePoint) / 1_000_000 + "ms");
+        System.out.println(factorial);
+
+
+        Fibonacci fibonacci = new Fibonacci(100, true);
+        timePoint = System.nanoTime();
+        fibonacci.calculate();
+        System.out.println("Elapsed Time: " + (System.nanoTime() - timePoint) / 1_000_000 + "ms");
+        System.out.println(fibonacci);
+    }
+
+}
---
+++ src/com/gojavaonline3/dlenchuk/finalproject/factorial/Factorial.java
@@ -0,0 +1,52 @@
+package com.gojavaonline3.dlenchuk.finalproject.factorial;
+
+import java.math.BigInteger;
+
+public class Factorial {
+
+    public static final int MAX_NUMBER_OF_FACTORIAL_ARGUMENT = 1000;
+
+    private int argument;
+    private BigInteger factorial;
+    private boolean calculated;
+
+    public Factorial(int argument) {
+        this.argument = argument;
+    }
+
+    public int getArgument() {
+        return argument;
+    }
+
+    public void setArgument(int argument) {
+        this.argument = argument;
+        calculated = false;
+        factorial = null;
+    }
+
+    public boolean isCalculated() {
+        return calculated;
+    }
+
+    public BigInteger calculate() {
+        if (calculated) {
+            return factorial;
+        }
+        if (argument > MAX_NUMBER_OF_FACTORIAL_ARGUMENT) {
+            throw new IllegalArgumentException("Argument of calculate function must be less or equals " +
+                    MAX_NUMBER_OF_FACTORIAL_ARGUMENT);
+        }
+        factorial = fact(BigInteger.valueOf(argument));
+        calculated = true;
+        return factorial;
+    }
+
+    private BigInteger fact(BigInteger number) {
+        return number.equals(BigInteger.ONE) ? number : number.multiply(fact(number.subtract(BigInteger.ONE)));
+    }
+
+    @Override
+    public String toString() {
+        return "Factorial(" + argument + ") = " + factorial;
+    }
+}
